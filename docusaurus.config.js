8f4403b350f99d1d1677057b3be39420274e20dc
David Sands <davidmaxsands@gmail.com>
2022-09-13 17:09:03 -0400

    removed links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -83,40 +83,19 @@ const config = {
           {
             title: 'Docs',
             items: [
-              {
-                label: 'Notes',
-                to: '/blog',
-              },
+             
             ],
           },
           {
             title: 'Community',
             items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
+            
             ],
           },
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
+              
             ],
           },
         ],
